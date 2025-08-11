5659ca7830d3245b3ddd0d7450673db14bbfc954
avinashcodelabs <avinashcodelabs@gmail.com>
2025-08-11 21:04:16 +0530

    added terminal chat app with context between chats

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("Hi txty")
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationSummaryMemory
+from dotenv import load_dotenv
+import warnings
+
+
+load_dotenv()
+
+warnings.filterwarnings("ignore")
+
+chat = ChatOpenAI(
+    model="deepseek-coder:6.7b",
+    openai_api_base="http://localhost:11434/v1",
+    openai_api_key="doesn't matter",
+    verbose=True
+)
+
+
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory(file_path="./messages.json"),
+    memory_key="messages",
+    return_messages=True,
+    llm=chat
+)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+    verbose=True
+)
+
+while True:
+    content = input(">> ")
+    if content == "bye":
+        break
+
+    result = chain({
+        "content": content,
+    })
+    print(result["text"])
